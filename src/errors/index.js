cd3a82cde725a15c8b184f02b6703fc2939f2d0e
Janez Urevc <janez@janezurevc.name>
2018-09-17 20:32:03 +0200

    Automated liting fixes to the files in src/errors.

--- src/errors/index.js
+++ src/errors/index.js
@@ -1,6 +1,6 @@
-'use strict';
 
-var _ = require('lodash');
+
+const _ = require('lodash');
 
 function format(message, args) {
   return message
@@ -8,16 +8,16 @@ function format(message, args) {
     .replace('{1}', args[1])
     .replace('{2}', args[2]);
 }
-var traverseNode = function(parent, errorDefinition) {
-  var NodeError = function() {
+const traverseNode = function (parent, errorDefinition) {
+  const NodeError = function () {
     if (_.isString(errorDefinition.message)) {
       this.message = format(errorDefinition.message, arguments);
     } else if (_.isFunction(errorDefinition.message)) {
       this.message = errorDefinition.message.apply(null, arguments);
     } else {
-      throw new Error('Invalid error definition for ' + errorDefinition.name);
+      throw new Error(`Invalid error definition for ${errorDefinition.name}`);
     }
-    this.stack = this.message + '\n' + (new Error()).stack;
+    this.stack = `${this.message}\n${(new Error()).stack}`;
   };
   NodeError.prototype = Object.create(parent.prototype);
   NodeError.prototype.name = parent.prototype.name + errorDefinition.name;
@@ -29,33 +29,34 @@ var traverseNode = function(parent, errorDefinition) {
 };
 
 /* jshint latedef: false */
-var childDefinitions = function(parent, childDefinitions) {
-  _.each(childDefinitions, function(childDefinition) {
+var childDefinitions = function (parent, childDefinitions) {
+  _.each(childDefinitions, (childDefinition) => {
     traverseNode(parent, childDefinition);
   });
 };
 /* jshint latedef: true */
 
-var traverseRoot = function(parent, errorsDefinition) {
+const traverseRoot = function (parent, errorsDefinition) {
   childDefinitions(parent, errorsDefinition);
   return parent;
 };
 
 
-var bitcore = {};
-bitcore.Error = function() {
+const bitcore = {};
+bitcore.Error = function () {
   this.message = 'Internal error';
-  this.stack = this.message + '\n' + (new Error()).stack;
+  this.stack = `${this.message}\n${(new Error()).stack}`;
 };
 bitcore.Error.prototype = Object.create(Error.prototype);
 bitcore.Error.prototype.name = 'bitcore.Error';
 
 
-var data = require('./spec');
+const data = require('./spec');
+
 traverseRoot(bitcore.Error, data);
 
 module.exports = bitcore.Error;
 
-module.exports.extend = function(spec) {
+module.exports.extend = function (spec) {
   return traverseNode(bitcore.Error, spec);
 };
