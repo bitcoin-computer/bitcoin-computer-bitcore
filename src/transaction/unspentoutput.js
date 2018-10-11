00219c63abe406eec72be03ca19567fe77e63b67
Janez Urevc <janez@janezurevc.name>
2018-10-11 17:20:45 +0200

    Convert src/transaction/unspentoutput.js to ES6 class syntax.

--- src/transaction/unspentoutput.js
+++ src/transaction/unspentoutput.js
@@ -22,80 +22,80 @@ const Unit = require('../unit');
  *   (1 BTC = 1e8 satoshis)
  * @param {string|Address=} data.address the associated address to the script, if provided
  */
-function UnspentOutput(data) {
-  /* jshint maxcomplexity: 20 */
-  /* jshint maxstatements: 20 */
-  if (!(this instanceof UnspentOutput)) {
+class UnspentOutput {
+  constructor(data) {
+    $.checkArgument(_.isObject(data), 'Must provide an object from where to extract data');
+    const address = data.address ? new Address(data.address) : undefined;
+    const txId = data.txid ? data.txid : data.txId;
+    if (!txId || !JSUtil.isHexaString(txId) || txId.length > 64) {
+      // TODO: Use the errors library
+      throw new Error('Invalid TXID in object', data);
+    }
+    const outputIndex = _.isUndefined(data.vout) ? data.outputIndex : data.vout;
+    if (!_.isNumber(outputIndex)) {
+      throw new Error(`Invalid outputIndex, received ${outputIndex}`);
+    }
+    $.checkArgument(!_.isUndefined(data.scriptPubKey) || !_.isUndefined(data.script),
+      'Must provide the scriptPubKey for that output!');
+    const script = new Script(data.scriptPubKey || data.script);
+    $.checkArgument(!_.isUndefined(data.amount) || !_.isUndefined(data.satoshis),
+      'Must provide an amount for the output');
+    const amount = !_.isUndefined(data.amount)
+      ? Unit.fromBTC(data.amount).toSatoshis() : data.satoshis;
+    $.checkArgument(_.isNumber(amount), 'Amount must be a number');
+    JSUtil.defineImmutable(this, {
+      address,
+      txId,
+      outputIndex,
+      script,
+      satoshis: amount,
+    });
+  }
+
+  /**
+   * Provide an informative output when displaying this object in the console
+   * @returns string
+   */
+  inspect() {
+    return `<UnspentOutput: ${this.txId}:${this.outputIndex
+      }, satoshis: ${this.satoshis}, address: ${this.address}>`;
+  }
+
+  /**
+   * String representation: just "txid:index"
+   * @returns string
+   */
+  toString() {
+    return `${this.txId}:${this.outputIndex}`;
+  }
+
+  /**
+   * Deserialize an UnspentOutput from an object
+   * @param {object|string} data
+   * @return UnspentOutput
+   */
+  static fromObject(data) {
     return new UnspentOutput(data);
   }
-  $.checkArgument(_.isObject(data), 'Must provide an object from where to extract data');
-  const address = data.address ? new Address(data.address) : undefined;
-  const txId = data.txid ? data.txid : data.txId;
-  if (!txId || !JSUtil.isHexaString(txId) || txId.length > 64) {
-    // TODO: Use the errors library
-    throw new Error('Invalid TXID in object', data);
+
+  /**
+   * Returns a plain object (no prototype or methods) with the associated info for this output
+   * @return {object}
+   */
+  toJSON() {
+    return {
+      address: this.address ? this.address.toString() : undefined,
+      txid: this.txId,
+      vout: this.outputIndex,
+      scriptPubKey: this.script.toBuffer().toString('hex'),
+      amount: Unit.fromSatoshis(this.satoshis).toBTC(),
+    };
   }
-  const outputIndex = _.isUndefined(data.vout) ? data.outputIndex : data.vout;
-  if (!_.isNumber(outputIndex)) {
-    throw new Error(`Invalid outputIndex, received ${outputIndex}`);
+
+  toObject() {
+    return this.toJSON();
   }
-  $.checkArgument(!_.isUndefined(data.scriptPubKey) || !_.isUndefined(data.script),
-    'Must provide the scriptPubKey for that output!');
-  const script = new Script(data.scriptPubKey || data.script);
-  $.checkArgument(!_.isUndefined(data.amount) || !_.isUndefined(data.satoshis),
-    'Must provide an amount for the output');
-  const amount = !_.isUndefined(data.amount)
-    ? Unit.fromBTC(data.amount).toSatoshis() : data.satoshis;
-  $.checkArgument(_.isNumber(amount), 'Amount must be a number');
-  JSUtil.defineImmutable(this, {
-    address,
-    txId,
-    outputIndex,
-    script,
-    satoshis: amount,
-  });
+
 }
-
-/**
- * Provide an informative output when displaying this object in the console
- * @returns string
- */
-UnspentOutput.prototype.inspect = function () {
-  return `<UnspentOutput: ${this.txId}:${this.outputIndex
-  }, satoshis: ${this.satoshis}, address: ${this.address}>`;
-};
-
-/**
- * String representation: just "txid:index"
- * @returns string
- */
-UnspentOutput.prototype.toString = function () {
-  return `${this.txId}:${this.outputIndex}`;
-};
-
-/**
- * Deserialize an UnspentOutput from an object
- * @param {object|string} data
- * @return UnspentOutput
- */
-UnspentOutput.fromObject = function (data) {
-  return new UnspentOutput(data);
-};
-
-/**
- * Returns a plain object (no prototype or methods) with the associated info for this output
- * @return {object}
- */
-UnspentOutput.prototype.toJSON = function toObject() {
-  return {
-    address: this.address ? this.address.toString() : undefined,
-    txid: this.txId,
-    vout: this.outputIndex,
-    scriptPubKey: this.script.toBuffer().toString('hex'),
-    amount: Unit.fromSatoshis(this.satoshis).toBTC(),
-  };
-};
-
-UnspentOutput.prototype.toObject = UnspentOutput.prototype.toJSON;
 
 module.exports = UnspentOutput;
--- test/transaction/unspentoutput.js
+++ test/transaction/unspentoutput.js
@@ -8,7 +8,7 @@ var expect = chai.expect;
 var bch = require('../..');
 var UnspentOutput = bch.Transaction.UnspentOutput;
 
-describe('UnspentOutput', function() {
+describe('UnspentOutput', function () {
 
   var sampleData1 = {
     'address': 'mszYqVnqKoQx4jcTdJXxwKAissE3Jbrrc1',
@@ -25,35 +25,31 @@ describe('UnspentOutput', function() {
     'amount': 0.01080000
   };
 
-  it('roundtrip from raw data', function() {
-    expect(UnspentOutput(sampleData2).toObject()).to.deep.equal(sampleData2);
+  it('roundtrip from raw data', function () {
+    expect(new UnspentOutput(sampleData2).toObject()).to.deep.equal(sampleData2);
   });
 
-  it('can be created without "new" operand', function() {
-    expect(UnspentOutput(sampleData1) instanceof UnspentOutput).to.equal(true);
-  });
-
-  it('fails if no tx id is provided', function() {
-    expect(function() {
+  it('fails if no tx id is provided', function () {
+    expect(function () {
       return new UnspentOutput({});
     }).to.throw();
   });
 
-  it('fails if vout is not a number', function() {
+  it('fails if vout is not a number', function () {
     var sample = _.cloneDeep(sampleData2);
     sample.vout = '1';
-    expect(function() {
+    expect(function () {
       return new UnspentOutput(sample);
     }).to.throw();
   });
 
-  it('displays nicely on the console', function() {
+  it('displays nicely on the console', function () {
     var expected = '<UnspentOutput: a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458:0' +
-                   ', satoshis: 1020000, address: mszYqVnqKoQx4jcTdJXxwKAissE3Jbrrc1>';
+      ', satoshis: 1020000, address: mszYqVnqKoQx4jcTdJXxwKAissE3Jbrrc1>';
     expect(new UnspentOutput(sampleData1).inspect()).to.equal(expected);
   });
 
-  describe('checking the constructor parameters', function() {
+  describe('checking the constructor parameters', function () {
     var notDefined = {
       'txId': 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458',
       'outputIndex': 0,
@@ -65,24 +61,24 @@ describe('UnspentOutput', function() {
       'script': 'OP_DUP OP_HASH160 20 0x88d9931ea73d60eaf7e5671efc0552b912911f2a OP_EQUALVERIFY OP_CHECKSIG',
       'amount': 0
     };
-    it('fails when no amount is defined', function() {
-      expect(function() {
+    it('fails when no amount is defined', function () {
+      expect(function () {
         return new UnspentOutput(notDefined);
       }).to.throw('Must provide an amount for the output');
     });
-    it('does not fail when amount is zero', function() {
-      expect(function() {
+    it('does not fail when amount is zero', function () {
+      expect(function () {
         return new UnspentOutput(zero);
       }).to.not.throw();
     });
   });
 
-  it('toString returns txid:vout', function() {
+  it('toString returns txid:vout', function () {
     var expected = 'a477af6b2667c29670467e4e0728b685ee07b240235771862318e29ddbe58458:0';
     expect(new UnspentOutput(sampleData1).toString()).to.equal(expected);
   });
 
-  it('to/from JSON roundtrip', function() {
+  it('to/from JSON roundtrip', function () {
     var utxo = new UnspentOutput(sampleData2);
     var obj = UnspentOutput.fromObject(utxo.toJSON()).toObject();
     expect(obj).to.deep.equal(sampleData2);
